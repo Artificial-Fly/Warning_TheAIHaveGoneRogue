7daf2a2861f91b1a92da367a171ce0aff0dbe505
Artificial-Fly <lazyfruit1@hotmail.com>
2024-08-16 18:49:41 +0300

    Fix Collision bug
    Fix bug when player character passes through obstacle

--- Assets/Components/ActorSensesBase.cs
+++ Assets/Components/ActorSensesBase.cs
@@ -21,10 +21,10 @@ public class ActorSensesBase : MonoBehaviour
             //Goal Ahead?
             Debug.Log("Goal Ahead");
         }else if(other.gameObject.tag=="Obstacle" || other.gameObject.tag=="Hitbox"){//pickups' tags: Pickup_Heal, Pickup_Timer, Pickup_Trap,
-                if(Mathf.Abs(OtherActorLocation.x-CurrentActorLocation.x)<15 && Mathf.Abs(OtherActorLocation.x-CurrentActorLocation.x)>9 && OtherActorLocation.x>CurrentActorLocation.x){RightSense=true;CurrentTriggeredSense = 1;}
-                if(Mathf.Abs(OtherActorLocation.x-CurrentActorLocation.x)<15 && Mathf.Abs(OtherActorLocation.x-CurrentActorLocation.x)>9 && OtherActorLocation.x<CurrentActorLocation.x){LeftSense=true;CurrentTriggeredSense = 2;}
-                if(Mathf.Abs(OtherActorLocation.y-CurrentActorLocation.y)<15 && Mathf.Abs(OtherActorLocation.y-CurrentActorLocation.y)>9 && OtherActorLocation.y>CurrentActorLocation.y){UpSense=true;CurrentTriggeredSense = 3;}
-                if(Mathf.Abs(OtherActorLocation.y-CurrentActorLocation.y)<15 && Mathf.Abs(OtherActorLocation.y-CurrentActorLocation.y)>9 && OtherActorLocation.y<CurrentActorLocation.y){DownSense=true;CurrentTriggeredSense = 4;}
+                if(Mathf.Abs(OtherActorLocation.x-CurrentActorLocation.x)<15 && Mathf.Abs(OtherActorLocation.x-CurrentActorLocation.x)>5 && OtherActorLocation.x>CurrentActorLocation.x){RightSense=true;CurrentTriggeredSense = 1;}
+                if(Mathf.Abs(OtherActorLocation.x-CurrentActorLocation.x)<15 && Mathf.Abs(OtherActorLocation.x-CurrentActorLocation.x)>5 && OtherActorLocation.x<CurrentActorLocation.x){LeftSense=true;CurrentTriggeredSense = 2;}
+                if(Mathf.Abs(OtherActorLocation.y-CurrentActorLocation.y)<15 && Mathf.Abs(OtherActorLocation.y-CurrentActorLocation.y)>5 && OtherActorLocation.y>CurrentActorLocation.y){UpSense=true;CurrentTriggeredSense = 3;}
+                if(Mathf.Abs(OtherActorLocation.y-CurrentActorLocation.y)<15 && Mathf.Abs(OtherActorLocation.y-CurrentActorLocation.y)>5 && OtherActorLocation.y<CurrentActorLocation.y){DownSense=true;CurrentTriggeredSense = 4;}
                 if(OnSensesTriggered!=null){
                     OnSensesTriggered(UpSense, DownSense, LeftSense, RightSense, other.gameObject.tag, CurrentTriggeredSense);
                 }
